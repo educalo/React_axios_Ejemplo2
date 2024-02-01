65a0f84eea76f3ab72dc1415c1c09a640a481bbc
educalo <64586573+educalo@users.noreply.github.com>
2024-02-01 10:39:45 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -17,9 +17,10 @@ let observador = new IntersectionObserver((entradas, observador) => {
 	threshold: 1.0
 });
 
-
+//llamada asincrona para obtener todas las peliculas
 const cargarPeliculas = async() => {
 	try {
+		//2.- utilizando axios
 		const respuesta = await axios.get('https://api.themoviedb.org/3/movie/popular', {
 			params: {
 				// api_key: '192e0b9821564f26f52949758ea3c473',
@@ -30,7 +31,8 @@ const cargarPeliculas = async() => {
 				'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOTJlMGI5ODIxNTY0ZjI2ZjUyOTQ5NzU4ZWEzYzQ3MyIsInN1YiI6IjYxODQyMWZlOGVkMDNmMDAyZDA4ZjZlZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nrCdKCx2dZQ7d0WaJpXJaPO_De2iP2rYg9bPon1O3V0'
 			}
 		})
-
+		
+		//1.- Utilizando fetch
 		// const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);
 	
 		// console.log(respuesta);
@@ -47,7 +49,8 @@ const cargarPeliculas = async() => {
 			});
 
 			document.getElementById('contenedor').innerHTML = peliculas;
-
+			
+			//scroll infinito video explicativo adjunto
 			if(pagina < 1000){
 				if(ultimaPelicula){
 					observador.unobserve(ultimaPelicula);
--- axios.js
+++ axios.js
@@ -1,3 +1,20 @@
+//1.- Defino get con promesas, arreglas llamada
+/*
+axios.get('https://api.themoviedb.org/3/movie/popular')
+	.then((respuesta) => {
+		console.log(respuesta.data.result[1].title)
+	})
+	.catch((error) => {
+		console.log(error);
+	})
+*/
+
+//2.- definimos una función asincrona llamada obtenerPeliculas
+//el resultado de la petición asincrona lo guardes en respuesta
+//en axios podemos pasar los parametros de la URL
+//los headers nos va a permitir autentificarnos con el servidor, en este caso estoy utilizando un token.
+//Si utilizo los token la api_key ya no es necesaria
+//me tengo que identificar en la pagina https://developer.themoviedb.org/reference/intro/authentication#api-key-quick-start para obtener los valores de api key o los token
 const obtenerPeliculas = async() => {
 	try {
 		const respuesta = await axios.get('https://api.themoviedb.org/3/movie/popular', {
@@ -17,4 +34,5 @@ const obtenerPeliculas = async() => {
 	
 }
 
+//aqui es donde llamo a la funcin obtenerPeliculas
 obtenerPeliculas();
